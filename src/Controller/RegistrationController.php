e95c5e70a18728e8cfbde242c31d0f2ffdfa218d
jeje <le-berre.jeremy@orange.fr>
2020-12-15 19:28:52 +0100

    Mailer

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -53,7 +53,7 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
 
             // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
+            /*$this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
                     ->from(new Address(
                         $this->getParameter('app_mail_from_address'),
@@ -62,7 +62,7 @@ class RegistrationController extends AbstractController
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
-            );
+            );*/
             // do anything else you need here, like send an email
 
             return $guardHandler->authenticateUserAndHandleSuccess(
