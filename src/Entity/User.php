c88680e7930efb921f27e0d95f7afa10d63001de
jeje <le-berre.jeremy@orange.fr>
2021-01-20 16:29:02 +0100

    entity user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -13,7 +13,6 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
- * @ORM\Table(name="`user`")
  * @ORM\HasLifecycleCallbacks
  * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
